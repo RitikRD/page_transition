d46fbe2068ff1a7f1e1b5299ec963cbdcedf3580
RitikRD <ritikdixit004@gmail.com>
2020-09-14 12:50:55 +0530

    Initial commit

---
+++ lib/pages/swipeleftpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Left extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.black,
+      appBar:
+      AppBar(title: Text("left",style: TextStyle(color: Colors.white),)),
+
+    );
+  }
+}
---
+++ lib/pages/swiperightpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Right extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(title: Text("right",style: TextStyle(),)),
+
+    );
+  }
+}
